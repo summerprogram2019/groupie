2f1b56f1aab5e2ac21f14e017242a3f0e3ba0476
BraeWebb <email@braewebb.com>
2019-07-03 09:06:37 +0800

    Login Screen UI

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:groupie/screens.dart' show WelcomeScreen;
 import 'package:groupie/widgets.dart' show GroupieLogo;
 
 class LoginScreen extends StatefulWidget {
@@ -16,12 +17,74 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final FocusNode focus = FocusNode();
 
+  /// Retrieves the text in the email entry field
+  final TextEditingController emailController = new TextEditingController();
+  /// Retrieves the text in the password entry field
+  final TextEditingController passwordController = new TextEditingController();
+
   bool _loggingIn = false;
+
+  String errors = "";
 
   @override
   Widget build(BuildContext context) {
 
     final logo = new GroupieLogo(() => FocusScope.of(context).requestFocus(focus));
+
+    // the email entry field
+    final email = TextFormField(
+        key: Key('email_field'),
+        keyboardType: TextInputType.emailAddress,
+        focusNode: focus,
+        controller: emailController,
+        decoration: InputDecoration(
+          labelText: "Email",
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 1.0,
+            ),
+          ),
+        ),
+    );
+
+    // password entry field
+    final password = TextFormField(
+        key: Key('password_field'),
+        autofocus: false,
+        obscureText: true,
+        controller: passwordController,
+        decoration: InputDecoration(
+          labelText: "Password",
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 1.0,
+            ),
+          ),
+        ),
+    );
+
+    final forgotLabel = FlatButton(
+      key: Key('forgot_password'),
+      child: Text(
+        'Forgot your password?',
+        style: TextStyle(color: Colors.blue),
+      ),
+      onPressed: () {
+        Navigator.of(context).pushNamed(WelcomeScreen.tag);
+      },
+    );
+
+    final signupButton = RaisedButton(
+      key: Key('signup_button'),
+      child: Text(
+        'Sign Up',
+        style: TextStyle(color: Colors.white),
+      ),
+      color: Colors.red,
+      onPressed: () {
+        Navigator.of(context).pushNamed(WelcomeScreen.tag);
+      },
+    );
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -36,10 +99,15 @@ class _LoginScreenState extends State<LoginScreen> {
                 logo,
                 // Sizedboxes are used for whitespace and padding on the screen
                 SizedBox(height: 48.0),
-//                email,
-//                SizedBox(height: 8.0),
-//                password,
-//                SizedBox(height: 24.0),
+                email,
+                SizedBox(height: 8.0),
+                password,
+                SizedBox(height: 24.0),
+                forgotLabel,
+                SizedBox(height: 24.0),
+                Text(errors),
+                SizedBox(height: 24.0),
+                signupButton
 //                loginButton,
 //                SizedBox(height: 8.0),
 //                newUser,
