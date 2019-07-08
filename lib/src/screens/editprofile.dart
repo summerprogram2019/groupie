24511fd417b450b4174490e82298d4c93f304b9f
Laura Davis <l.davis2@uqconnect.edu.au>
2019-07-08 16:39:26 +1000

    Up to date work (Laura) for Eliza

--- lib/src/screens/editprofile.dart
+++ lib/src/screens/editprofile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:groupie/util.dart' show GroupieColours;
+import 'package:groupie/screens.dart' show HomePage;
 
 /*
   Initiates the edit profile screen
@@ -22,18 +23,48 @@ _EditProfileState createState() => new _EditProfileState();
 class _EditProfileState extends State<EditProfile> {
   _EditProfileState() : super();
 
+  final changeProfileImage = (context) => RaisedButton(
+    key: Key('changeprofileimage_button'),
+    shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+    color: Colors.white,
+    child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Text(
+          'Change Profile Picture',
+          style: TextStyle(color: GroupieColours.grey69,
+              fontSize: 26.0),
+        )),
+    onPressed: () {
+      Navigator.of(context).pushNamed(HomePage.tag);
+    },
+  );
+
+  final setName = TextFormField(
+    key: Key('name_field'),
+    keyboardType: TextInputType.text,
+    decoration: InputDecoration(
+      labelText: "Username",
+      border: OutlineInputBorder(
+        borderSide: BorderSide(
+          width: 1.0,
+        ),
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context){
     return new Scaffold(
         appBar: new AppBar(
-        title: new Text("Edit Profile",
-        style: new TextStyle(
-          color: GroupieColours.grey69,
-          fontSize: 20.0,)),
-    backgroundColor: GroupieColours.white69,
-    ),
-      backgroundColor: GroupieColours.white69,
-      body: ListView(
+            title: new Text("Edit Profile",
+                style: new TextStyle(
+                  color: GroupieColours.grey69,
+                  fontSize: 20.0,)),
+            backgroundColor: GroupieColours.white69,
+            iconTheme: new IconThemeData(color: GroupieColours.grey69),
+        ),
+        backgroundColor: GroupieColours.white69,
+        body: ListView(
         children: <Widget>[
           new Row(
             children: <Widget>[
@@ -51,9 +82,37 @@ class _EditProfileState extends State<EditProfile> {
                           )
                       )
                   )
-              )
+              ),
             ],
           ),
+          changeProfileImage(context),
+          SizedBox(height: 12.0),
+          new Column(
+            children: [
+              Text("Set Profile Name",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(color: GroupieColours.grey69,
+                    fontSize: 30.0)),
+              setName,
+              SizedBox(height: 6.0),
+              Text("Set Bio",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(color: GroupieColours.grey69,
+                      fontSize: 30.0)),
+              Text("Set Location",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(color: GroupieColours.grey69,
+                      fontSize: 30.0)),
+              Text("Set Phone Number",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(color: GroupieColours.grey69,
+                      fontSize: 30.0)),
+              Text("Set Email",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(color: GroupieColours.grey69,
+                      fontSize: 30.0)),
+          ]
+          )
         ],
       )
     );
