ac79062fabb0d395ef2214d7d2ae7283bad7b521
BraeWebb <email@braewebb.com>
2019-07-11 13:55:07 +0800

    Error Handling & Snackbar

--- lib/src/screens/homescreen.dart
+++ lib/src/screens/homescreen.dart
@@ -23,7 +23,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Map<int, Widget> cards = {};
+  Widget _cardScreen = new Container();
 
   bool _loading = true;
 
@@ -33,27 +33,20 @@ class _HomePageState extends State<HomePage> {
 
     getEvents().then((events) {
       setState(() {
-        cards = buildCards(events);
+        _cardScreen = new CardsScreen(events);
+        _loading = false;
+      });
+    }).catchError((error) {
+      setState(() {
+        _cardScreen = new Center(
+          child: Text("Error loading events"),
+        );
         _loading = false;
       });
     });
   }
 
-  Map<int, Widget> buildCards(List<Event> events) {
-    Map<int, Widget> eventWidgets = {};
 
-    for (Event event in events) {
-      eventWidgets[event.id] = EventCard(event,
-        remove: () {
-          setState(() {
-            eventWidgets.remove(event.id);
-          });
-        },
-      );
-    }
-
-    return eventWidgets;
-  }
 
   //navigator functions to change the screen
   void _openProfile() {
@@ -103,45 +96,85 @@ class _HomePageState extends State<HomePage> {
       ),
       body: new LoadableScreen(
         visible: !_loading,
-        child: new Stack(
-          children: [
-            new Center(
-              child: new Stack(
-                alignment: Alignment.center,
-                children: cards.values.toList()
-              ),
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Flexible(
-                  child: DragTarget(
-                    builder: (a, b, c) => Container(width: 150),
-                    onAccept: (data) {
-                      print(data);
-                      print("FUCK NO");
-                    },
-                  )
-                ),
-                Spacer(),
-                Flexible(
-                  child: DragTarget(
-                    builder: (a, b, c) => Container(width: 150),
-                    onAccept: (data) {
-                      print(data);
-                      print("FUCK YES");
-                    },
-                  )
-                ),
-              ],
-            )
-          ]
-        )
+        child: _cardScreen
       ),
       floatingActionButton: new FloatingActionButton(onPressed: () {
         Navigator.of(context).pushNamed(ParticipantsScreen.tag);
       }),
+    );
+  }
+}
+
+
+class CardsScreen extends StatefulWidget {
+  final List<Event> events;
+
+  CardsScreen(this.events) : super();
+
+  @override
+  _CardsScreenState createState() {
+    return new _CardsScreenState(events);
+  }
+}
+
+class _CardsScreenState extends State<CardsScreen> {
+  Map<int, Widget> _cards;
+
+  final acceptSnack = SnackBar(content: Text('Event accepted!'));
+  final rejectSnack = SnackBar(content: Text('Event rejected!'));
+
+  _CardsScreenState(List<Event> events) : super() {
+    Map<int, Widget> eventWidgets = {
+      -1: new Center(child: new Text("Nothing to display"))
+    };
+
+    for (Event event in events) {
+      eventWidgets[event.id] = EventCard(event,
+        remove: () {
+          setState(() {
+            eventWidgets.remove(event.id);
+          });
+        },
+      );
+    }
+
+    _cards = eventWidgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+        children: [
+          new Center(
+            child: new Stack(
+                alignment: Alignment.center,
+                children: _cards.values.toList()
+            ),
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                  child: DragTarget(
+                    builder: (a, b, c) => Container(width: 150),
+                    onAccept: (data) {
+                      Scaffold.of(context).showSnackBar(rejectSnack);
+                    },
+                  )
+              ),
+              Spacer(),
+              Flexible(
+                  child: DragTarget(
+                    builder: (a, b, c) => Container(width: 150),
+                    onAccept: (data) {
+                      Scaffold.of(context).showSnackBar(acceptSnack);
+                    },
+                  )
+              ),
+            ],
+          )
+        ]
     );
   }
 }
